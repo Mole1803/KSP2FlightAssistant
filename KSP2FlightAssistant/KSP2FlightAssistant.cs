f3abec9cab63ff60e4feb371f332b2df78cdeb9e
Moritz_Hartmann <W8w6V35zC3q2A7mQyhMD>
2023-03-11 22:43:43 +0100

    build: rebuild project struct

---
+++ KSP2FlightAssistant/KSP2FlightAssistant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using KSP;
+using KSP.Game;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Logging;
+using SpaceWarp.API.Mods;
+using SpaceWarp;
+using SpaceWarp.API.UI;
+
+
+namespace KSP2FlightAssistant
+{
+    [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    public class KSP2FlightAssistant : BaseSpaceWarpPlugin
+    {
+        // Mod info
+        public const string ModGuid = "com.github.Mole1803.KSP2FlightAssistant";
+        public const string ModName = "KSP2FlightAssistant";
+        public const string ModVer = "1.0.0";
+        private static KSP2FlightAssistant Instance { get; set; }
+
+        public override void OnInitialized()
+        {
+            base.OnInitialized();
+            Instance = this;
+        }
+    }
+
+}
---
+++ KSP2FlightAssistant/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KSP2FlightAssistant")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KSP2FlightAssistant")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("518EA0A5-3D4A-404C-AC7B-3ED3BC254062")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
